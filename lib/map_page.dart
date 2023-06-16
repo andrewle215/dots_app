c8c83f3528a997cf498cd50031b9cb01b596533b
andrewle215 <andrewle2003@gmail.com>
2023-06-16 13:07:55 -0400

    v1.7

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geocoding/geocoding.dart';
+
 
 class MyPage extends StatelessWidget {
   const MyPage({Key? key}) : super(key: key);
@@ -52,3 +54,74 @@ class _MapScreenState extends State<MapScreen> {
     });
   }
 }
+
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: MapSample(),
+//     );
+//   }
+// }
+//
+// class MapSample extends StatefulWidget {
+//   @override
+//   State<MapSample> createState() => MapSampleState();
+// }
+//
+// class MapSampleState extends State<MapSample> {
+//   late GoogleMapController mapController;
+//   late Marker marker;
+//   late Polygon parkingArea;
+//
+// //  MapSampleState({required this.mapController, required this.marker, required this.parkingArea});
+//
+//   static const LatLng initialPosition = const LatLng(38.9974, 76.9320);
+//   Set<Marker> markers = {};
+//
+//   // Define your hardcoded significant locations here
+//   final List<Location> locations = [
+//     Location(name: "Library", latitude: 38.9974, longitude: 76.9320),
+//     // Add more locations as needed
+//   ];
+//
+//   void _onMapCreated(GoogleMapController controller) {
+//     mapController = controller;
+//
+//     // Create a marker for each location and add it to the markers set
+//     for (final location in locations) {
+//       final locationMarker = Marker(
+//         markerId: MarkerId(location.name),
+//         position: LatLng(location.latitude, location.longitude),
+//         infoWindow: InfoWindow(
+//           title: location.name,
+//         ),
+//         icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+//       );
+//       markers.add(locationMarker);
+//     }
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     throw UnimplementedError();
+//   }
+//
+// // Remaining code from the previous example...
+//
+// }
+//
+// class Location {
+//   final String name;
+//   final double latitude;
+//   final double longitude;
+//
+//   Location(
+//       {required this.name,required this.latitude,required this.longitude}
+//       );
+// }
